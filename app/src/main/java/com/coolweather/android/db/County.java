c187797d9aa3d53c97801dd45634c839738940f6
“用 <2628481641@qq.com>
2020-05-28 16:26:25 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,53 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    //县的名字
+    private String weatherId;
+    //县对应的天气id
+    private  int cityId;
+    //当前县所属市的id值
+    //当前县所属市的id值
+
+    public County(int id, String countyName, String weatherId, int cityId) {
+        this.id = id;
+        this.countyName = countyName;
+        this.weatherId = weatherId;
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
